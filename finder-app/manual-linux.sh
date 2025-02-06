14afa64e90b5b9c4d6b070151f838a411ee3548a
jauy2310 <jauy2310@colorado.edu>
2025-02-06 15:57:47 -0700

    troubleshooting 'unable to mount rootfs...'

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+TOOLCHAIN_SYSROOT=/opt/arm/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc
 
 if [ $# -lt 1 ]
 then
@@ -22,6 +23,11 @@ else
 fi
 
 mkdir -p ${OUTDIR}
+
+if [ $? -ne 0 ]; then
+	# exit with failure if directory could not be created
+	exit -1
+fi
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
@@ -35,9 +41,20 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+	echo "make mrproper..."
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+	echo "make defconfig..."
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+	echo "make all..."
+	make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+	echo "make modules..."
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+	echo "make dtbs..."
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +65,11 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +78,64 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+	make distclean
+	make defconfig
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+# www.gnu.org/software/sed/manual/sed.html#sed-regular-expressions to figure out how to extract a string from output of previous command
+# Asked ChatGPT on how to approach the multi-line output of the shared libraries line
+INTERPRETER_PATH=$(${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter" | sed -E 's/\[.*: \/(.*)\]/\1/')
+mkdir -p "$(dirname $INTERPRETER_PATH)"
+INTERPRETER_PATH=$(echo $INTERPRETER_PATH | tr -d "[:space:]")
+FULL_INTERPRETER_PATH="${TOOLCHAIN_SYSROOT}/${INTERPRETER_PATH}"
+cp $FULL_INTERPRETER_PATH ${OUTDIR}/rootfs/lib
+
+SHARED_LIBS=$(${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library" | sed -E 's/.*\[(.*)]/\1/')
+for CURR_LIB in $SHARED_LIBS; do
+	cp ${TOOLCHAIN_SYSROOT}/lib64/${CURR_LIB} ${OUTDIR}/rootfs/lib64
+done
 
 # TODO: Make device nodes
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/console c 5 1
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/zero c 1 5
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/tty c 5 0
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/tty0 c 4 0
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/tty1 c 4 1
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/random c 1 8
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/urandom c 1 9
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/ram0 b 1 0
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/initrd b 1 250
 
 # TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make writer
+cp writer ${OUTDIR}/rootfs/home/
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/../conf/assignment.txt ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/../conf/username.txt ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home/
 
 # TODO: Chown the root directory
+sudo chown -R root:root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
+find ${OUTDIR}/rootfs | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
