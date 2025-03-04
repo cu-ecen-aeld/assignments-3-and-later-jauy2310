f027b685d9c8ab02e209ade691bd140165933ca0
jauy2310 <jauy2310@colorado.edu>
2025-03-03 21:46:15 -0500

    Refactoring code

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,12 +1,11 @@
 #include "aesdsocket.h"
 
+/**************************************************************************************************
+ * MAIN
+ **************************************************************************************************/
 int main(int argc, char *argv[]) {
-    // open syslog
-    openlog(NULL, LOG_PID | LOG_CONS | LOG_NDELAY, LOG_USER);
-
-    // signal handler
-    signal(SIGINT, signal_handler);
-    signal(SIGTERM, signal_handler);
+    // initialize server functions
+    initialize_server();
 
     // return values for functions
     int rc = 0;
@@ -47,81 +46,13 @@ int main(int argc, char *argv[]) {
     rc = listen(server_socket_fd, NUM_CONNECTIONS);
     if (rc == -1) goto exit_socket_listen;
 
-    // set aside client variables accessible outside of loop (for cleanup)
-    struct sockaddr client_address_info;
-    socklen_t client_address_len = sizeof(client_address_info);
-    char client_ip[INET_ADDRSTRLEN];
-
     // start daemon if -d flag was passed
     start_daemon(argc, argv);
 
-    // main loop; keep accepting/closing connections 
-    while (1) {
-        // create client address info struct
-        syslog(LOG_INFO, "Accepting socket connection.");
-        rc = accept(server_socket_fd, (struct sockaddr *)&client_address_info, &client_address_len);
-        if (rc == -1) goto exit_socket_accept;
-        client_fd = rc;
-
-        // log client connection
-        struct sockaddr_in *client = (struct sockaddr_in *)&client_address_info;
-        inet_ntop(client->sin_family, &client->sin_addr, client_ip, sizeof(client_ip));
-        syslog(LOG_INFO, "Accepted connection from %s", client_ip);
-
-        // open file to write
-        tmpdata_fd = open(TMPDATA_PATH, O_APPEND | O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP); 
-        if (tmpdata_fd == -1) goto retry;
-
-        // read_buffer to store incoming data
-        char read_buffer[BUFFER_SIZE] = {0};
-
-        // write buffer to store outgoing data
-        char write_buffer[BUFFER_SIZE] = {0};
-        int write_buffer_index = 0;
-
-        // infinite loop to process incoming data while connection is open
-        while (1) {
-            // receive data from socket
-            ssize_t bytes_received = recv(client_fd, read_buffer, BUFFER_SIZE, 0);
-
-            // client connection closed
-            if (bytes_received <= 0) break;
-
-            // for loop to process a single sub-buffer
-            for (int i = 0; i < bytes_received; i++) {
-                if (read_buffer[i] == '\n') {
-                    // packet completed; send to file
-                    syslog(LOG_DEBUG, "Packet complete. Data: %s", write_buffer);
-
-                    // reset write buffer and write to file
-                    lseek(tmpdata_fd, 0, SEEK_END);
-                    write(tmpdata_fd, write_buffer, write_buffer_index);
-                    write(tmpdata_fd, "\n", 1);
-                    memset(write_buffer, 0, BUFFER_SIZE);
-                    write_buffer_index = 0;
-
-                    // packet was received; send entire contents of file to client
-                    lseek(tmpdata_fd, 0, SEEK_SET);
-                    char file_content[BUFFER_SIZE];
-                    size_t bytes_read;
-                    while ((bytes_read = read(tmpdata_fd, file_content, sizeof(file_content))) > 0) {
-                        send(client_fd, file_content, bytes_read, 0);
-                    }
-                } else {
-                    write_buffer[write_buffer_index++] = read_buffer[i];
-                }
-            }
-        }
-
-retry:
-        syslog(LOG_INFO, "Closed connection from %s", client_ip);
-        close(client_fd);
-        close(tmpdata_fd);
-    }
+    // accept connections - main program loop
+    accept_connections();
 
 // cleanup labels; makes it easier to read code and keep track of frees/closes
-exit_socket_accept:
-    if (rc == -1) syslog(LOG_ERR, "Exiting socket accept.");
 exit_socket_listen:
     if (rc == -1) syslog(LOG_ERR, "Exiting socket listen.");
 exit_socket_bind:
@@ -138,13 +69,306 @@ exit_free_addrinfo_struct:
     return rc;
 }
 
+
+/**************************************************************************************************
+ * FUNCTION DEFINITIONS - SERVER
+ **************************************************************************************************/
+void initialize_server() {
+    // open syslog
+    openlog(NULL, LOG_PID | LOG_CONS | LOG_NDELAY, LOG_USER);
+
+    // signal handler
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+}
+
+void accept_connections() {
+    while (1) {
+        // create client address info
+        struct sockaddr client_address_info;
+        socklen_t client_address_len = sizeof(client_address_info);
+        char client_ip[INET_ADDRSTRLEN];
+
+        // create client address info struct
+        syslog(LOG_INFO, "Accepting socket connection.");
+        int client_fd = accept(server_socket_fd, (struct sockaddr *)&client_address_info, &client_address_len);
+        if (client_fd == -1) {
+            syslog(LOG_ERR, "accept() failed.");
+            continue;
+        }
+
+        // log client connection
+        struct sockaddr_in *client = (struct sockaddr_in *)&client_address_info;
+        inet_ntop(client->sin_family, &client->sin_addr, client_ip, sizeof(client_ip));
+        syslog(LOG_INFO, "Accepted connection from %s", client_ip);
+
+        // create a new entry
+        thread_entry_t *new_connection = thread_entry_create(0, client_ip, client_fd);
+        if (new_connection == NULL) {
+            syslog(LOG_ERR, "Error malloc'ing memory for new thread entry");
+            continue;
+        }
+
+        // create a new pthread
+        if (pthread_create(&new_connection->thread_id, NULL, client_handler, new_connection) != 0 ) {
+            close(client_fd);
+            thread_entry_free(new_connection);
+            continue;
+        }
+
+        // add thread to thread manager
+        thread_entry_add(new_connection);
+
+        // check if any of the current threads need to be joined
+        thread_entry_t *current_entry = NULL;
+        SLIST_FOREACH(current_entry, &thread_manager, entries) {
+            if (current_entry->is_complete) {
+                pthread_join(current_entry->thread_id, NULL);
+            }
+        }
+    }
+}
+
+void *client_handler(void *arg) {
+    // define the client fd
+    thread_entry_t *connection = (thread_entry_t *)arg;
+    pthread_t thread_id = connection->thread_id;
+    int client_fd = connection->client_fd;
+
+    // print
+    syslog(LOG_DEBUG, "New connection:");
+    thread_entry_print(connection);
+    
+    // open file to write
+    int tmpdata_client_fd = open(TMPDATA_PATH, O_APPEND | O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP); 
+    if (tmpdata_client_fd == -1) {
+        syslog(LOG_ERR, "Failed to open %s", TMPDATA_PATH);
+        close(client_fd);
+        return NULL;
+    }
+
+    // read_buffer to store incoming data
+    char read_buffer[BUFFER_SIZE] = {0};
+
+    // write buffer to store outgoing data
+    char write_buffer[BUFFER_SIZE] = {0};
+    int write_buffer_index = 0;
+
+    // infinite loop to process incoming data while connection is open
+    while (1) {
+        // receive data from socket
+        ssize_t bytes_received = recv(client_fd, read_buffer, BUFFER_SIZE, 0);
+
+        // client connection closed
+        if (bytes_received <= 0) {
+            if (connection->client_ip != NULL) {
+                syslog(LOG_INFO, "Closed client connection from %s.", (char *)connection->client_ip);
+            } else {
+                syslog(LOG_INFO, "Closed client connection from unknown.");
+            }
+            break;
+        }
+
+        // for loop to process a single sub-buffer
+        for (int i = 0; i < bytes_received; i++) {
+            if (read_buffer[i] == '\n') {
+                // packet completed; send to file
+                syslog(LOG_DEBUG, "Packet complete. Data: %s", write_buffer);
+
+                // lock mutex
+                pthread_mutex_lock(&file_mutex);
+
+                // reset write buffer and write to file
+                lseek(tmpdata_client_fd, 0, SEEK_END);
+                write(tmpdata_client_fd, write_buffer, write_buffer_index);
+                write(tmpdata_client_fd, "\n", 1);
+
+                // unlock mutex
+                pthread_mutex_unlock(&file_mutex);
+
+                // reset write buffer
+                memset(write_buffer, 0, BUFFER_SIZE);
+                write_buffer_index = 0;
+
+                // packet was received; send entire contents of file to client
+                lseek(tmpdata_client_fd, 0, SEEK_SET);
+                char file_content[BUFFER_SIZE];
+                size_t bytes_read;
+                while ((bytes_read = read(tmpdata_client_fd, file_content, sizeof(file_content))) > 0) {
+                    send(client_fd, file_content, bytes_read, 0);
+                }
+            } else {
+                write_buffer[write_buffer_index++] = read_buffer[i];
+            }
+        }
+    }
+
+    // mark thread as complete
+    thread_entry_markcomplete(thread_id);
+
+    // cleanup
+    close(tmpdata_client_fd);
+    close(client_fd);
+    return NULL;
+}
+
+/**************************************************************************************************
+ * THREAD MANAGER - Tracks threads for entire application
+ **************************************************************************************************/
+thread_entry_t *thread_entry_create(pthread_t new_thread_id, const char *new_client_ip, int new_client_fd) {
+    // allocate memory
+    thread_entry_t *new_thread_entry = (thread_entry_t *)malloc(sizeof(thread_entry_t));
+    if (!new_thread_entry) {
+        syslog(LOG_ERR, "Error malloc'ing thread_entry");
+        return NULL;
+    }
+
+    // assign fields to new thread entry
+    new_thread_entry->thread_id = new_thread_id;
+    new_thread_entry->client_ip = strdup(new_client_ip);
+    if (!new_thread_entry->client_ip) {
+        syslog(LOG_ERR, "Error malloc'ing thread_entry->client_ip");
+        free(new_thread_entry);
+        return NULL;
+    }
+    new_thread_entry->client_fd = new_client_fd;
+    new_thread_entry->is_complete = false;
+
+    // return
+    return new_thread_entry;
+}
+
+int thread_entry_free(thread_entry_t *entry) {
+    // check if entry is already null
+    if (!entry) {
+        syslog(LOG_ERR, "Thread being free'd is already NULL");
+        return -1;
+    }
+
+    // free string in struct
+    if (entry->client_ip != NULL) {
+        free(entry->client_ip);
+    }
+
+    // free struct malloc
+    free(entry);
+
+    // return success
+    return 0;
+}
+
+int thread_entry_add(thread_entry_t *entry) {
+    // check if entry is valid
+    if (!entry) {
+        syslog(LOG_ERR, "Thread entry to add is NULL");
+        return -1;
+    }
+
+    // add the thread entry
+    pthread_mutex_lock(&manager_mutex);
+    SLIST_INSERT_HEAD(&thread_manager, entry, entries);
+    pthread_mutex_unlock(&manager_mutex);
+
+    // return 
+    return 0;
+}
+
+int thread_entry_remove(pthread_t thread_id) {
+    // malloc a current node to use for checking
+    thread_entry_t *current_entry = NULL;
+
+    // lock the manager
+    pthread_mutex_lock(&manager_mutex);
+    SLIST_FOREACH(current_entry, &thread_manager, entries) {
+        if (current_entry->thread_id == thread_id) {
+            // remove entry from list
+            SLIST_REMOVE(&thread_manager, current_entry, thread_entry_t, entries);
+
+            // free the removed struct
+            thread_entry_free(current_entry);
+
+            // unlock mutex before returning
+            pthread_mutex_unlock(&manager_mutex);
+
+            // return
+            return 0;
+        }
+    }
+    pthread_mutex_unlock(&manager_mutex);
+
+    // no thread_ids matched
+    return -1;
+}
+
+int thread_entry_markcomplete(pthread_t thread_id) {
+    // malloc a current node to use for checking
+    thread_entry_t *current_entry = NULL;
+
+    // lock the manager
+    pthread_mutex_lock(&manager_mutex);
+    SLIST_FOREACH(current_entry, &thread_manager, entries) {
+        if (current_entry->thread_id == thread_id) {
+            // set the is_complete field for this thread to true
+            current_entry->is_complete = true;
+
+            // unlock mutex before returning
+            pthread_mutex_unlock(&manager_mutex);
+
+            // return
+            return 0;
+        }
+    }
+    pthread_mutex_unlock(&manager_mutex);
+
+    // no thread_ids matched
+    return -1;
+}
+
+void thread_entry_freeall() {
+    // create a current node to use for checking
+    thread_entry_t *current_entry = NULL;
+
+    while(!(SLIST_EMPTY(&thread_manager))) {
+        // get head of thread manager
+        current_entry = SLIST_FIRST(&thread_manager);
+
+        // remove this from thread manager
+        thread_entry_remove(current_entry->thread_id);
+    }
+}
+
+void thread_entry_print(thread_entry_t *current_entry) {
+    // print info
+    syslog(LOG_DEBUG, "Thread ID: %d | Client IP: %s | Client FD: %d | Completion Status: %s\n",
+        (int)current_entry->thread_id, current_entry->client_ip, current_entry->client_fd, current_entry->is_complete ? "Yes" : "No");
+}
+
+void thread_entry_printall() {
+    // create a current node to use for checking
+    thread_entry_t *current_entry = NULL;
+
+    // print header
+    syslog(LOG_DEBUG, "===> THREAD MANAGER HEAD\n");
+
+    // lock the manager
+    pthread_mutex_lock(&manager_mutex);
+    SLIST_FOREACH(current_entry, &thread_manager, entries) {
+        thread_entry_print(current_entry);
+    }
+    pthread_mutex_unlock(&manager_mutex);
+
+    // print footer
+    syslog(LOG_DEBUG, "===> THREAD MANAGER TAIL\n");
+}
+
+/**************************************************************************************************
+ * FUNCTIONS - SIGNAL HANDLER
+ **************************************************************************************************/
 void signal_handler() {
     // log signal handler, using re-entrant write() instead of a normal printf/log
     syslog(LOG_INFO, "Caught signal, exiting");
 
     // attempt to close files
-    close(tmpdata_fd);
-    close(client_fd);
     close(server_socket_fd);
 
     // attempt to free addrinfo struct
@@ -157,6 +381,9 @@ void signal_handler() {
     exit(1);
 }
 
+/**************************************************************************************************
+ * FUNCTIONS - DAEMON
+ **************************************************************************************************/
 void start_daemon(int argc, char **argv) {
     // check command line options with getopt()
     // https://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html
--- server/aesdsocket.h
+++ server/aesdsocket.h
@@ -1,3 +1,7 @@
+/**************************************************************************************************
+ * INCLUDES
+ **************************************************************************************************/
+
 // include standard libraries
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,21 +19,58 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
+// queue
+#include <sys/queue.h>
+
+// multithreading
+#include <pthread.h>
+
+/**************************************************************************************************
+ * CONSTANTS AND GLOBALS
+ **************************************************************************************************/
+
 // define constants
 #define PORT                "9000"
-#define NUM_CONNECTIONS     1
+#define NUM_CONNECTIONS     10
 #define TMPDATA_PATH        "/var/tmp/aesdsocketdata"
 #define BUFFER_SIZE         1024 * 1024
 
-// file descriptors
-static int tmpdata_fd;
+// server socket file descriptor
 static int server_socket_fd;
-static int client_fd;
 
 // server addrinfo
 struct addrinfo *server_address_info;
 
-// function prototypes
+// tmpdata file mutex
+pthread_mutex_t file_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/**************************************************************************************************
+ * FUNCTION PROTOTYPES
+ **************************************************************************************************/
+/**
+ * initialize_server()
+ * 
+ * Calls functions to initialize utilities, such as syslog, signal handlers, etc.
+ * 
+ * @return none
+ */
+void initialize_server();
+
+/**
+ * accept_connections()
+ * 
+ * Main program loop that accepts connections and spawns client threads
+ * 
+ * @return none
+ */
+void accept_connections();
+
+/**
+ * client_handler()
+ * 
+ * Threading function to handle a client connection
+ */
+void *client_handler(void *arg);
 
 /**
  * signal_handler()
@@ -53,3 +94,125 @@ void signal_handler();
  */
 void start_daemon(int argc, char **argv);
 
+
+/**************************************************************************************************
+ * THREAD MANAGER - Tracks threads for entire application
+ **************************************************************************************************/
+
+/**
+ * struct thread_entry_t
+ * 
+ * @brief holds a single thread's information, meant to be used in a linked list (SLIST)
+ */
+typedef struct thread_entry_t {
+    pthread_t                       thread_id;          // thread ID
+    char *                          client_ip;          // client IP address
+    int                             client_fd;          // client connection fd
+    bool                            is_complete;        // thread completion boolean
+    SLIST_ENTRY(thread_entry_t)     entries;            // next thread entry
+} thread_entry_t;
+
+// define a single head of the linked list, called thread_manager
+SLIST_HEAD(thread_manager_t, thread_entry_t) thread_manager;
+
+// mutex for thread_manager
+pthread_mutex_t manager_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/**
+ * thread_entry_create()
+ * 
+ * Creates a new thread entry that will be used to track the threads and corresponding client info
+ * 
+ * @param new_thread_id             Thread ID
+ * @param new_client_ip             Client's IP address
+ * @param new_client_fd             File descriptor to access client connection
+ * 
+ * @return new thread entry
+ */
+thread_entry_t *thread_entry_create(pthread_t new_thread_id, const char *new_client_ip, int new_client_fd);
+
+/**
+ * thread_entry_copy()
+ * 
+ * Creates a copy of a thread entry
+ * 
+ * @note caller must free() this copy
+ * 
+ * @param copy_thread               Thread entry to copy
+ * 
+ * @return the malloc'd copy of the thread entry
+ */
+thread_entry_t *thread_entry_copy(thread_entry_t *copy_thread);
+
+/**
+ * thread_entry_free()
+ * 
+ * Frees a thread entry
+ * 
+ * @param entry                     Thread entry to free
+ * 
+ * @return 0 on success, -1 on failure
+ */
+int thread_entry_free(thread_entry_t *entry);
+
+/**
+ * thread_entry_add()
+ * 
+ * Adds a thread entry to the thread manager
+ * 
+ * @param entry                     Thread entry to add
+ * 
+ * @return 0 on success, -1 on failure
+ */
+int thread_entry_add(thread_entry_t *entry);
+
+/**
+ * thread_entry_remove()
+ * 
+ * Removes a thread entry from the thread manager
+ * 
+ * @param thread_id                 The entry containing the thread_id to remove
+ * 
+ * @return 0 on success, -1 on failure
+ */
+int thread_entry_remove(pthread_t thread_id);
+
+/**
+ * thread_entry_markcomplete()
+ * 
+ * Marks a thread in the thread manager as complete
+ * 
+ * @param thread_id                 The entry containing the thread_id to remove
+ * 
+ * @return 0 on success, -1 on failure
+ */
+int thread_entry_markcomplete(pthread_t thread_id);
+
+/**
+ * thread_entry_freeall()
+ * 
+ * Frees all the threads in the thread manager
+ * 
+ * @return none
+ */
+void thread_entry_freeall();
+
+/**
+ * thread_entry_print()
+ * 
+ * Prints thread entry info
+ * 
+ * @param current_entry             The entry to print
+ * 
+ * @return none
+ */
+void thread_entry_print(thread_entry_t *current_entry);
+
+/**
+ * thread_entry_printall()
+ * 
+ * Prints all threads in thread manager
+ * 
+ * @return none
+ */
+void thread_entry_printall();
